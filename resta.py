e62048eb6a599e127407dcef365ed5dbd0267628
Gaboo2001 <gh053965@gmail.com>
2025-05-19 10:02:02 -0600

    Agregar método de resta

---
+++ resta.py
@@ -0,0 +1,10 @@
+def restar(a, b):
+    return a - b
+
+
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+
+resultado = restar(num1, num2)
+print("La resta es:", resultado)
