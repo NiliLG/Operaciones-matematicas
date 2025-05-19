c05336beef4fca9747ab5027116063133f154931
Elton Yael Alba Roldan <147437882+EltonYaelAlbaRoldan@users.noreply.github.com>
2025-05-19 10:14:13 -0600

    Agregar método de multiplicacion

---
+++ Multiplicacion.py
@@ -0,0 +1,8 @@
+def multiplicar (a, b):
+    return a * b
+
+num1 =float(input("Ingresar el primmer numero: "))
+num2 =float(input("Ingresar el segundo numero: "))
+
+resultado = multiplicar(num1, num2)
+print("El resultado es", resultado)
